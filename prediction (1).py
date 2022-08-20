d363703622f10eafdfeb5100842bccdddc12cf90
shubhangi12345678 <97526096+shubhangi12345678@users.noreply.github.com>
2022-08-20 19:57:41 +0530

    Add files via upload

---
+++ prediction (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+pickle_in = open('NB_model.pkl', 'rb') 
+NB_model = pickle.load(pickle_in)
+
+pickle_in = open('LG_model.pkl', 'rb') 
+LG_model = pickle.load(pickle_in)
+
+pickle_in = open('DT_model.pkl', 'rb') 
+DT_model = pickle.load(pickle_in)
+
+
+
+
+
+def get_prediction(data,model):
+    """
+    Predict the class of a given data point.
+    """
+    return model.predict(data)
+
